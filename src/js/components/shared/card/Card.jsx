e591b8a903b36ae02e25111ae0b0b0a52ba4754f
Adam Morsi <adam.morsi@yahoo.com>
2021-10-25 22:18:38 +0300

    add use tilt custom hook

--- src/js/components/shared/card/Card.jsx
+++ src/js/components/shared/card/Card.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 //styles
 import styles from './Card.scss';
+//custom hooks
+import useTilt from '../../../customHooks/UseTilt';
 
-const Card = ({ children, classes, doubleClickHandler }) => (
-	<div
-		onDoubleClick={doubleClickHandler ? doubleClickHandler : () => {}}
-		className={`${styles.shadowCard} ${classes ? classes : ''}`}
-	>
-		{children}
-	</div>
-);
+const Card = ({ children, classes, doubleClickHandler }) => {
+	const ref = useTilt();
+
+	return (
+		<div
+			ref={ref}
+			onDoubleClick={doubleClickHandler ? doubleClickHandler : () => {}}
+			className={`${styles.shadowCard} ${classes ? classes : ''}`}
+		>
+			{children}
+		</div>
+	);
+};
 
 Card.propTypes = {
 	classes: PropTypes.string,
